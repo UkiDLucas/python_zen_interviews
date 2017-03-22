63743fc7d85799bd6674bc8c61df4e021117e310
Uki D. Lucas <UkiDLucas@gmail.com>
2017-03-22 07:26:07 -0500

    best practices

---
+++ best_practices.py
@@ -0,0 +1,32 @@
+
+# coding: utf-8
+
+# # Readability over compactness
+# 
+# In Python community, including the official documentation and examples, there is a tendency to use most compact and esoteric syntax possible. This does not serve anyone. I, as a hiring manager, do not want to hire people who pride themselves in writing an unreadable code. This tendency also shows immaturity, the orgin of that tendency can be two-fold: 
+# 
+# - the original authors were loner-hacker types that only cared to write a short scripts that served themselves
+# - the original authors were academics that never worked for an extended amount of time in large teams
+# 
+# Maintaining an unreadable code is a nightmare and often it is easier to re-write it from scratch.
+# 
+# Finally, I believe the code should be self-commenting and comments should be used only when the developer exhausts all possible opitions to make their code more readable.
+# 
+# The classes and functions should have description and examples of how should they be used.
+
+# # Python Naming Conventions:
+# 
+# - **Modules** should have short, **all-lowercase** names, underscores can be used. 
+# - **Packages** should have short, **all-lowercase** names, underscores are discouraged.
+# - **Classes** should normally use the **CapWords** convention.
+# - **Constants** should be written in **UPPER_CASE_WITH_UNDERSCORES**
+# - **Variables** should be written in **lower_case_with_underscores**
+# 
+# Read more at:
+# 
+# https://www.python.org/dev/peps/pep-0008/
+
+# In[ ]:
+
+
+
--- bubble_sort.py
+++ bubble_sort.py
@@ -1,23 +1,26 @@
 
 # coding: utf-8
 
-# In[1]:
+# # Bubble sort
 
-# Bubble sort
+# 
 
-
-# In[4]:
+# In[8]:
 
 def bubble_sort(alist: list):
+    print("elements to sort", len(alist))
     temp_list = []
+    iteration = 0
     
     # step backwards thru the list
     start = len(alist)-1
     stop = 0
     step = -1
-    for passnum in range(start, stop, step):
-        for i in range(passnum):
-            if alist[i]>alist[i+1]:
+    for placeholder in range(start, stop, step):
+        for i in range(placeholder):
+            iteration = iteration + 1
+            print("iteration", iteration, "placeholder", placeholder, "index", i)
+            if alist[i] > alist[i+1]:
                 temp = alist[i]
                 alist[i] = alist[i+1]
                 alist[i+1] = temp
