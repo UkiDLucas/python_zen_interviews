3adc81c6e2a785dd27b56d7db1c5535d9f00b209
Uki Lucas <ulucas@productiveedge.com>
2017-04-13 15:33:28 -0500

    prep

--- prep.py
+++ prep.py
@@ -1,39 +1,37 @@
 
 # coding: utf-8
 
-# # Big-O notations 
-# 
-# also known as "the run time characteristic of an algorithm".  You may want to refresh hash tables, heaps, binary trees, linked lists, depth-first search, recursion. For more information on Algorithms you can visit:
-# http://www.topcoder.com/tc?module=Static&d1=tutorials&d2=alg_index
-# 
-# 
-# 
-# 
-
 # # Coding
 # 
 # You should know at least one programming language really well (preferably C++,  Java or Python). You will be expected to write some code in at least some of your interviews. You will be expected to know a fair amount of detail about your preferred programming language and will be asked to do some coding on the whiteboard.
-
-# # Sorting
-# 
-# Know how to sort. Don't do bubble-sort. You should know the details of at least one n*log(n) sorting algorithm, preferably two (say, quicksort and merge sort). Merge sort can be highly useful in situations where quick sort is impractical, so take a look at it.
 
 # # Hashtables
 # 
 # Arguably the single most important data structure known to mankind. You absolutely should know how they work. Be able to implement one using only arrays in your favorite language, in about the space of one interview.
 
+# # Sorting
+# 
+# Know how to sort. Don't do bubble-sort. You should know the details of at least one n*log(n) sorting algorithm, preferably two (say, quicksort and merge sort). Merge sort can be highly useful in situations where quick sort is impractical, so take a look at it.
+
 # # Trees
 # 
 # Know about trees; basic tree construction, traversal and manipulation algorithms. Familiarize yourself with binary trees, n-ary trees, and trie-trees. Be familiar with at least one type of balanced binary tree, whether it's a red/black tree, a splay tree or an AVL tree, and know how it's implemented. Understand tree traversal
+
+# # Other Data Structures
+# 
+# You should study up on as many other data structures and algorithms as possible. You should especially know about the most famous classes of NP-complete problems, such as traveling salesman and the knapsack problem, and be able to recognize them when an interviewer asks you them in disguise. Find out whatNP-complete means.
 
 # # Algorithms
 # 
 # BFS and DFS, and know the difference between inorder, postorder and preorder.
 # Graphs: Graphs are really important at Google. There are 3 basic ways to represent a graph in memory (objects and pointers, matrix, and adjacency list); familiarize yourself with each representation and its pros & cons. You should know the basic graph traversal algorithms: breadth-first search and depth-first search. Know their computational complexity, their tradeoffs, and how to implement them in real code. If you get a chance, try to study up on fancier algorithms, such as Dijkstra and A*.
-
-# # Other Data Structures
 # 
-# You should study up on as many other data structures and algorithms as possible. You should especially know about the most famous classes of NP-complete problems, such as traveling salesman and the knapsack problem, and be able to recognize them when an interviewer asks you them in disguise. Find out whatNP-complete means.
+# You may want to refresh hash tables, heaps, binary trees, linked lists, depth-first search, recursion. For more information on Algorithms you can visit:
+# http://www.topcoder.com/tc?module=Static&d1=tutorials&d2=alg_index
+
+# # Big-O notations 
+# 
+# also known as "the run time characteristic of an algorithm".  
 
 # # Mathematics
 # 
